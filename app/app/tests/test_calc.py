812a4fa29d4f4205afb2452db1ab9541de778c56
David <>
2024-04-04 20:57:54 -0700

    wait for db

--- app/app/tests/test_calc.py
+++ app/app/tests/test_calc.py
@@ -12,3 +12,8 @@ class CalcTests(SimpleTestCase):
         """Test adding numbers together."""
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
+
+    def test_add_2(self):
+        """Test adding numbers together."""
+        res = calc.add(1, 1)
+        self.assertEqual(res, 2)
